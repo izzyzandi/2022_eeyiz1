957dcb928d21edc37534af86555611266703f0fc
Izzy Zandi <eeyiz1@nottingham.ac.uk>
2023-02-14 11:06:40 +0000

    created cmake static and dynamic library projects

---
+++ worksheet4/calc/include/adder.h
@@ -0,0 +1,13 @@
+// begin adder.h
+// following lines stops this from being included more than once in each source
+
+#ifndef MATHSLIB_ADDER_H
+	#define MATHSLIB_ADDER_H
+
+	int add(int a, int b);
+
+
+	
+#endif
+
+//end adder.h
